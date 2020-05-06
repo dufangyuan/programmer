48a0b603d9b21dc0a674d4851d47ac6ab4c91b9f
Your Name <you@example.com>
2020-05-06 18:19:50 +0800

    login_DFY

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,22 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
+import login from '@/views/dfy/login'
 
 Vue.use(Router)
 
 export default new Router({
+  mode:"history",//去掉url中的#符号
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      redirect:"login"
+    },
+    {
+      //引入路径
+      path: '/login',
+      name: 'login',
+      component: login
     }
   ]
 })
